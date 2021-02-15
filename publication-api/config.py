b4134c1847f7143196afbc9c136e635c8592c227
Alessandro Zinni <alessandro.zinni98@icloud.com>
2021-02-15 09:08:58 +0100

    Added new API

---
+++ publication-api/config.py
@@ -0,0 +1,15 @@
+department = {
+    "Carla Piazza": "rp00339",
+    "Alberto Policriti": "rp01207",
+    "Agostino Dovier": "rp03621",
+    "Marino Miculan": "rp00250",
+    "Nicola Gigante": "rp21680",
+    "Nicola Vitacolonna": "rp03284"
+}
+
+bibtex_elements = ["authors", "title", "year", "journal", "handle",
+                   "publication type", "publisher", "volume", "book_title",
+                   "doi", "pages", "series"]
+
+markdown_elements = ["title", "authors", "year", "doi", "publication type",
+                     "abstract", "pdf"]
